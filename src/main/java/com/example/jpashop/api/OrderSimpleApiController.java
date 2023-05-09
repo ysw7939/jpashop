36edc5d1ecaa918443c28151b404e0873ba41711
yangsuwon <ysw7939@gmail.com>
2023-05-10 01:30:56 +0900

    JPA 활용 2 섹션 3. 지연 로딩과 조회 성능 최적화

--- src/main/java/com/example/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/example/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package com.example.jpashop.api;
 
+import com.example.jpashop.domain.Address;
 import com.example.jpashop.domain.Order;
+import com.example.jpashop.domain.OrderStatus;
 import com.example.jpashop.repository.OrderRepository;
 import com.example.jpashop.repository.OrderSearch;
+import com.example.jpashop.repository.OrderSimpleQueryDto;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToone(ManyToOne, OneToOne)
@@ -29,5 +35,44 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
+    }
+    @Data
+    private class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
--- src/main/java/com/example/jpashop/repository/OrderRepository.java
+++ src/main/java/com/example/jpashop/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.example.jpashop.repository;
 
+import com.example.jpashop.api.OrderSimpleApiController;
 import com.example.jpashop.domain.Member;
 import com.example.jpashop.domain.Order;
 import jakarta.persistence.EntityManager;
@@ -86,5 +87,22 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+       return em.createQuery(
+                "select new com.example.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address )"+
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
 
 }
